72dc43ea593f53eed7b623393314ccb4bfec2d3e
Pony <871999566@qq.com>
2025-05-24 11:56:25 +0800

    5.0.3

--- GravityEngineSDK.xcframework/ios-arm64/GravityEngineSDK.framework/Headers/GravityEngineSDK.h
+++ GravityEngineSDK.xcframework/ios-arm64/GravityEngineSDK.framework/Headers/GravityEngineSDK.h
@@ -124,7 +124,7 @@ typedef void (^CallbackWithError)(NSError * error);
 
 - (void)initializeGravityEngineWithClientId:(NSString *) clientId withUserName:(NSString *)userName withVersion:(int)version withAsaEnable:(bool)enableAsa withIdfa:(NSString *) idfa withIdfv:(NSString *)idfv withCaid1:(NSString *)caid1_md5 withCaid2:(NSString *)caid2_md5 withSyncAttribution:(bool)syncAttribution withCreateTime:(long)createTimestamp withCompany:(NSString *)company withChannel:(NSString *)channel withSuccessCallback:(CallbackWithSuccess)successCallback withErrorCallback:(CallbackWithError)errorCallback;
 
-- (void)initializeGravityEngineWithAsaEnable:(bool)enableAsa withCaid1:(NSString *)caid1_md5 withCaid2:(NSString *)caid2_md5 withSyncAttribution:(bool)syncAttribution withChannel:(NSString *)channel withSuccessCallback:(CallbackWithSuccess)successCallback withErrorCallback:(CallbackWithError)errorCallback;
+- (void)initializeGravityEngineWithAsaEnable:(bool)enableAsa withClientId:(NSString *)client_id withCaid1:(NSString *)caid1_md5 withCaid2:(NSString *)caid2_md5 withSyncAttribution:(bool)syncAttribution withChannel:(NSString *)channel withSuccessCallback:(CallbackWithSuccess)successCallback withErrorCallback:(CallbackWithError)errorCallback;
 
 - (void)queryUserInfoWithSuccessCallback:(void(^)(NSDictionary* _Nonnull data))successCallback withErrorCallback:(CallbackWithError)errorCallback;
 
